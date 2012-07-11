e9ed35f4b98de6a485326cddda0906bfa0f658c2
Jessica Bylo Chacon <bylochacon@gmail.com>
2012-07-11 11:38:47 -0700

    reorganization

---
+++ Why_the_Name_files/Why_the_Name.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20120711
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWPhotoFrame([IWCreateImage('Why_the_Name_files/Creme_frame2_01.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_02.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_04.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_08.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_09.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_14.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_13.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_05.png')],null,0,0.400000,3.000000,3.000000,3.000000,3.000000,23.000000,23.000000,23.000000,23.000000,20.000000,19.000000,20.000000,19.000000,null,null,null,0.400000),stroke_0:new IWPhotoFrame([IWCreateImage('Why_the_Name_files/Creme_frame2_01.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_02.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_04.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_08.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_09.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_14.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_13.png'),IWCreateImage('Why_the_Name_files/Creme_frame2_05.png')],null,0,0.400000,3.000000,3.000000,3.000000,3.000000,23.000000,23.000000,23.000000,23.000000,20.000000,19.000000,20.000000,19.000000,null,null,null,0.400000),shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(2.8284,2.8284),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Why_the_Name_files/Why_the_NameMoz.css')
+adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
